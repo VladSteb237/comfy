ab7edd4bf949e8554b3c07e89ca4162ce40691e8
VladSteb237 <vladsteb@mail.ru>
2026-02-25 17:43:30 +0300

    check CheckoutPage page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+export const dynamic = "force-dynamic";
+
 import { useSearchParams } from "next/navigation";
 import { loadStripe } from "@stripe/stripe-js";
 import { useCallback } from "react";
@@ -18,19 +21,18 @@ const CheckoutPage = () => {
   const cartId = searchParams.get("cartId");
 
   const fetchClientSecret = useCallback(async () => {
-    // Create a Checkout Session
     const response = await axios.post("/api/payment", {
-      orderId: orderId,
-      cartId: cartId,
+      orderId,
+      cartId,
     });
     return response.data.clientSecret;
-  }, []);
-
-  const options = { fetchClientSecret };
+  }, [orderId, cartId]);
 
   return (
     <div id="checkout">
-      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+      <EmbeddedCheckoutProvider
+        stripe={stripePromise}
+        options={{ fetchClientSecret }}>
         <EmbeddedCheckout />
       </EmbeddedCheckoutProvider>
     </div>
@@ -38,3 +40,44 @@ const CheckoutPage = () => {
 };
 
 export default CheckoutPage;
+////////////////////////////////////////////////////////////////////////////////
+// "use client";
+// import { useSearchParams } from "next/navigation";
+// import { loadStripe } from "@stripe/stripe-js";
+// import { useCallback } from "react";
+// import axios from "axios";
+// import {
+//   EmbeddedCheckoutProvider,
+//   EmbeddedCheckout,
+// } from "@stripe/react-stripe-js";
+
+// const stripePromise = loadStripe(
+//   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
+// );
+
+// const CheckoutPage = () => {
+//   const searchParams = useSearchParams();
+//   const orderId = searchParams.get("orderId");
+//   const cartId = searchParams.get("cartId");
+
+//   const fetchClientSecret = useCallback(async () => {
+//     // Create a Checkout Session
+//     const response = await axios.post("/api/payment", {
+//       orderId: orderId,
+//       cartId: cartId,
+//     });
+//     return response.data.clientSecret;
+//   }, []);
+
+//   const options = { fetchClientSecret };
+
+//   return (
+//     <div id="checkout">
+//       <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+//         <EmbeddedCheckout />
+//       </EmbeddedCheckoutProvider>
+//     </div>
+//   );
+// };
+
+// export default CheckoutPage;
