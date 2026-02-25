afccf852c9aec62890eaaef7db4b4b57ff02c36b
VladSteb237 <vladsteb@mail.ru>
2026-02-25 17:01:35 +0300

    change prisma.ts file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -13,7 +13,7 @@
 // export const prisma = new PrismaClient({ adapter });
 
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "../src/generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
