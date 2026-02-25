1891e7a6c9730527848aa854389981ada821c81a
VladSteb237 <vladsteb@mail.ru>
2026-02-25 13:53:35 +0300

    new Prisma config

--- lib/db.ts
+++ lib/db.ts
@@ -20,19 +20,36 @@
 // export default prisma;
 //////////////////////////////////////////////////////////////////////////////
 // lib/db.ts
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+
+// export const db =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     adapter,
+//   });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+/////////////////////////////////////////////////////////////////////////////
+
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const db =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    }),
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
